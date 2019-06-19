94a1c9e9166c3455062493c5036dfa071c086ecd
li leng <lengli@12thman.cn>
2019-06-19 16:33:36 +0800

    no message

--- lengli_learn/lengli_learn/AppDelegate.m
+++ lengli_learn/lengli_learn/AppDelegate.m
@@ -17,7 +17,10 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    ViewController *vc = [[ViewController alloc] init];
+    self.window.rootViewController = vc;
+    [self.window makeKeyAndVisible];
     // Override point for customization after application launch.
     return YES;
 }
