91645db741f6cc2eeba5bfea2cd8c054442aee09
li leng <lengli@12thman.cn>
2019-06-19 16:27:18 +0800

    no message

---
+++ lengli_learn/Pods/Target Support Files/Pods-lengli_learn/Pods-lengli_learn-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_lengli_learn : NSObject
+@end
+@implementation PodsDummy_Pods_lengli_learn
+@end
---
+++ lengli_learn/Pods/Target Support Files/Pods-lengli_learnTests/Pods-lengli_learnTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_lengli_learnTests : NSObject
+@end
+@implementation PodsDummy_Pods_lengli_learnTests
+@end
